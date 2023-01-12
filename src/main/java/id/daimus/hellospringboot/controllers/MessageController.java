548c1d110bc4c971258d3359c2bccfd5e3aac55e
Muhammad Daimus Suudi <daimus.suudi@gmail.com>
2023-01-12 21:10:32 +0700

    first commit

---
+++ src/main/java/id/daimus/hellospringboot/controllers/MessageController.java
@@ -0,0 +1,26 @@
+package id.daimus.hellospringboot.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+@RequestMapping("/v1")
+public class MessageController {
+    @GetMapping(path = "/messages")
+    public ResponseEntity<String> getMessage(){
+        log.info("GET /v1/messages called");
+        return ResponseEntity.ok("Hello World");
+    }
+    @PostMapping(path = "/messages")
+    public ResponseEntity<String> postMessage(){
+        log.info("POST /v1/messages called");
+        return ResponseEntity.ok("Hello World");
+    }
+    @DeleteMapping(path = "/messages")
+    public ResponseEntity<String> deleteMessage(){
+        log.info("DELETE /v1/messages called");
+        return ResponseEntity.ok("Hello World");
+    }
+}
